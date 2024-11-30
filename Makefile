d9abb48439fa471ede71e1232d2f5d94613dd7e7
significantnose <yanucevich.d@gmail.com>
2024-11-30 23:56:07 +0300

    [feat]: filtering implemented

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 obj-m += filterchip.o
-filterchip-y := fchip_codec.o fchip_posfix.o fchip_vga.o fchip_hda_bus.o fchip_int.o fchip_pcm.o fchip.o
+filterchip-y := fchip_codec.o fchip_posfix.o fchip_vga.o fchip_hda_bus.o fchip_int.o fchip_filter.o fchip.o
+
+KBUILD_CFLAGS += -msse -msse2 -msse4.1 -msse4.2
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
---
+++ fchip_filter.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#define FCHIP_FPARAM_FILTERTYPE_NOCHANGE -1
+#define FCHIP_FPARAM_SAMPLERATE_NOCHANGE -1
+#define FCHIP_FPARAM_CUTOFF_NOCHANGE -1
+
+enum fchip_filter_type{
+    FCHIP_FILTER_NONE,
+    FCHIP_FILTER_LOWPASS,
+    FCHIP_FILTER_HIPASS,
+    FCHIP_FILTER_BANDPASS,
+};
+
+// a biquad convolution table is used 
+// in the current implementation
+struct fchip_conv_table
+{
+    double b0;
+    double b1;
+    double b2;
+
+    double a1;
+    double a2;
+};
+
+struct fchip_channel_filter
+{
+    struct fchip_conv_table coeffs;
+    double raw[3];
+    double processed[3];
+
+    enum fchip_filter_type filter_type;
+    int sample_rate;
+    double cutoff_freq;
+};
+
+
+struct fchip_channel_filter* fchip_filter_init(enum fchip_filter_type type, int sample_rate, double cutoff_freq);
+void fchip_filter_change_params(struct fchip_channel_filter *filter, enum fchip_filter_type filter_type, double sample_rate, double cutoff_freq);
+
+inline double fchip_filter_process(struct fchip_channel_filter* filter, double sample);
