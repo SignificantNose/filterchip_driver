a08f192deface1c011f8cbc965a4bedd20988bdf
significantnose <yanucevich.d@gmail.com>
2024-12-03 09:11:09 +0300

    [feat]: filter float data type introduced, changed from double to float

--- fchip_filter.h
+++ fchip_filter.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #define FCHIP_FPARAM_FILTERTYPE_NOCHANGE -1
-#define FCHIP_FPARAM_SAMPLERATE_NOCHANGE -1
-#define FCHIP_FPARAM_CUTOFF_NOCHANGE -1
+#define FCHIP_FPARAM_SAMPLERATE_NOCHANGE -1.0f
+#define FCHIP_FPARAM_CUTOFF_NOCHANGE -1.0f
+
+typedef float fchip_float_t;
 
 enum fchip_filter_type{
     FCHIP_FILTER_NONE,
@@ -15,27 +17,27 @@ enum fchip_filter_type{
 // in the current implementation
 struct fchip_conv_table
 {
-    double b0;
-    double b1;
-    double b2;
+    fchip_float_t b0;
+    fchip_float_t b1;
+    fchip_float_t b2;
 
-    double a1;
-    double a2;
+    fchip_float_t a1;
+    fchip_float_t a2;
 };
 
 struct fchip_channel_filter
 {
     struct fchip_conv_table coeffs;
-    double raw[3];
-    double processed[3];
+    fchip_float_t raw[3];
+    fchip_float_t processed[3];
 
     enum fchip_filter_type filter_type;
     int sample_rate;
-    double cutoff_freq;
+    fchip_float_t cutoff_freq;
 };
 
 
-struct fchip_channel_filter* fchip_filter_create(enum fchip_filter_type filter_type, int sample_rate, double cutoff_freq);
-void fchip_filter_change_params(struct fchip_channel_filter *filter, enum fchip_filter_type filter_type, double sample_rate, double cutoff_freq);
+struct fchip_channel_filter* fchip_filter_create(enum fchip_filter_type filter_type, int sample_rate, fchip_float_t cutoff_freq);
+void fchip_filter_change_params(struct fchip_channel_filter *filter, enum fchip_filter_type filter_type, fchip_float_t sample_rate, fchip_float_t cutoff_freq);
 
-inline double fchip_filter_process(struct fchip_channel_filter* filter, double sample);
+inline fchip_float_t fchip_filter_process(struct fchip_channel_filter* filter, fchip_float_t sample);
